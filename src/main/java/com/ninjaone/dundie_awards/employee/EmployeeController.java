e496a4d6e17741e15bc84b590160638c0c9968cf
Dom <5888934+dominic-jones@users.noreply.github.com>
2024-10-19 10:38:32 +0900

    Swapping multiple ResponseBody annotations for RestController

--- src/main/java/com/ninjaone/dundie_awards/employee/EmployeeController.java
+++ src/main/java/com/ninjaone/dundie_awards/employee/EmployeeController.java
@@ -5,13 +5,12 @@ import com.ninjaone.dundie_awards.activity.MessageBroker;
 import com.ninjaone.dundie_awards.organization.AwardsCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping()
 public class EmployeeController {
 
@@ -29,28 +28,24 @@ public class EmployeeController {
 
     // get all employees
     @GetMapping("/employees")
-    @ResponseBody
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
 
     // create employee rest api
     @PostMapping("/employees")
-    @ResponseBody
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeService.createEmployee(employee);
     }
 
     // get employee by id rest api
     @GetMapping("/employees/{id}")
-    @ResponseBody
     public ResponseEntity<Employee> getEmployeeById(@PathVariable long id) {
         return employeeService.getEmployee(id);
     }
 
     // update employee rest api
     @PutMapping("/employees/{id}")
-    @ResponseBody
     public ResponseEntity<Employee> updateEmployee(@PathVariable Long id,
                                                    @RequestBody Employee employeeDetails) {
         return employeeService.updateEmployee(id, employeeDetails);
@@ -58,7 +53,6 @@ public class EmployeeController {
 
     // delete employee rest api
     @DeleteMapping("/employees/{id}")
-    @ResponseBody
     public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id) {
         return employeeService.deleteEmployee(id);
     }
