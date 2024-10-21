98fcbb541d4f7b76d6adc41774b866335d313b1d
Dom <5888934+dominic-jones@users.noreply.github.com>
2024-10-21 17:01:07 +0900

    Adding a JPA entity listener that posts a notification when a new employee has joined the company

---
+++ src/main/java/com/ninjaone/dundie_awards/employee/EmployeeListener.java
@@ -0,0 +1,35 @@
+package com.ninjaone.dundie_awards.employee;
+
+import com.ninjaone.dundie_awards.activity.MessageBroker;
+import jakarta.persistence.PostPersist;
+import org.springframework.context.annotation.Lazy;
+
+import static com.ninjaone.dundie_awards.activity.Message.createMessageNow;
+import static java.lang.String.format;
+
+public class EmployeeListener {
+
+    private final MessageBroker messageBroker;
+
+    public EmployeeListener(@Lazy MessageBroker messageBroker) {
+        this.messageBroker = messageBroker;
+    }
+
+    /**
+     * Currently also alerts for all the employees setup in DataLoader.
+     * If we're loading from the db on startup, we wouldn't want this.
+     * However, it makes for a nice demo.
+     */
+    @PostPersist
+    void onNewEmployee(Employee employee) {
+        messageBroker.sendMessage(
+                createMessageNow(
+                        format(
+                                "Employee %s %s has joined the company!",
+                                employee.getFirstName(),
+                                employee.getLastName()
+                        )
+                )
+        );
+    }
+}
