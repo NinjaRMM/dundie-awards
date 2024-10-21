245d7f37882de9336fd95e32a89f69b92a486d84
Dom <5888934+dominic-jones@users.noreply.github.com>
2024-10-21 15:07:57 +0900

    Setting up a configuration for the message buffer size

--- src/main/java/com/ninjaone/dundie_awards/DundieAwardsApplication.java
+++ src/main/java/com/ninjaone/dundie_awards/DundieAwardsApplication.java
@@ -2,8 +2,10 @@ package com.ninjaone.dundie_awards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class DundieAwardsApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/ninjaone/dundie_awards/config/DundieProperties.java
@@ -0,0 +1,15 @@
+package com.ninjaone.dundie_awards.config;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@ConfigurationProperties("dundie")
+public record DundieProperties(
+        @Min(1)
+        @NotNull
+        int messageBrokerSize
+) {
+}
