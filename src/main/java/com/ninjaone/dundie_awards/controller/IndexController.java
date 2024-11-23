09abc9ddd1518e25efa116d95924bebb93c141ce
fmarinhodebr <Filipe.MarinhoDeBrito@harman.com>
2024-11-23 00:29:46 -0300

    Add live reload feature
    Tied with 'dev' profile

--- src/main/java/com/ninjaone/dundie_awards/controller/IndexController.java
+++ src/main/java/com/ninjaone/dundie_awards/controller/IndexController.java
@@ -1,14 +1,16 @@
 package com.ninjaone.dundie_awards.controller;
 
-import com.ninjaone.dundie_awards.AwardsCache;
-import com.ninjaone.dundie_awards.MessageBroker;
-import com.ninjaone.dundie_awards.repository.ActivityRepository;
-import com.ninjaone.dundie_awards.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ninjaone.dundie_awards.AwardsCache;
+import com.ninjaone.dundie_awards.MessageBroker;
+import com.ninjaone.dundie_awards.repository.ActivityRepository;
+import com.ninjaone.dundie_awards.repository.EmployeeRepository;
 
 
 @Controller
@@ -26,6 +28,9 @@ public class IndexController {
 
     @Autowired
     private AwardsCache awardsCache;
+    
+    @Value("${spring.profiles.active:}")
+    private String activeProfile;
 
     @GetMapping()
     public String getIndex(Model model) {
@@ -33,6 +38,7 @@ public class IndexController {
         model.addAttribute("activities", activityRepository.findAll());
         model.addAttribute("queueMessages", messageBroker.getMessages());
         model.addAttribute("totalDundieAwards", awardsCache.getTotalAwards());
+        model.addAttribute("isDev", "dev".equalsIgnoreCase(activeProfile));
         return "index";
     }
 }
