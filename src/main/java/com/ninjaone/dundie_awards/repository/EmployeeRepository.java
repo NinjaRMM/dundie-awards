ca52bd7e863b0dd35defcc0856276b42bb7659da
Matthew Pitts <invinity@users.noreply.github.com>
2025-02-05 22:21:38 +0000

    initial work from codespaces

--- src/main/java/com/ninjaone/dundie_awards/repository/EmployeeRepository.java
+++ src/main/java/com/ninjaone/dundie_awards/repository/EmployeeRepository.java
@@ -1,10 +1,13 @@
 package com.ninjaone.dundie_awards.repository;
 
 import com.ninjaone.dundie_awards.model.Employee;
+import com.ninjaone.dundie_awards.model.Organization;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
+    @JpaQuery("update Employee e set e.dundieAwards = e.dundieAwards + {awardCount} where e.organization = {organization}")
+    int giveDundieAwardsByOrganization(Organization organization, int awardCount);
 }
---
+++ src/test/java/com/ninjaone/dundie_awards/repository/EmployeeRepositoryTest.java
@@ -0,0 +1,8 @@
+package com.ninjaone.dundie_awards.repository;
+
+class EmployeeRepositoryTest {
+    @Test
+    void giveDundieAwardsByOrganization_should_update_only_employees_in_the_specified_org() {
+        
+    }
+}
